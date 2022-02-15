29c3647d02d51495c680bd09c18930886602b4d0
woals990605 <woals990605@naver.com>
2022-02-15 11:19:09 +0900

    전이중채팅

--- src/site/metacoding/chat/MyClientSocket.java
+++ src/site/metacoding/chat/MyClientSocket.java
@@ -1,22 +1,59 @@
 package site.metacoding.chat;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.Scanner;
+
+import javax.sql.rowset.spi.SyncResolver;
 
 public class MyClientSocket {
 
     Socket socket;
     BufferedWriter writer;
+    Scanner sc;
+
+    // 추가
+    BufferedReader reader;
 
     public MyClientSocket() {
         try {
             // IP주소, 포트번호
-            socket = new Socket("192.168.0.132", 1077);
+            socket = new Socket("localhost", 1077);
             writer = new BufferedWriter(
                     new OutputStreamWriter(socket.getOutputStream()));
-            writer.write("황재민\n");
-            writer.flush();
+            sc = new Scanner(System.in);
+
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String inputData = reader.readLine();
+                        System.out.println("받은 메시지 : " + inputData);
+                        System.out.println("===============");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }).start();
+
+            System.out.println("내용을 입력하세요.");
+            while (true) {
+                String inputData = sc.nextLine();
+                writer.write(inputData + "\n");
+                writer.flush();
+                System.out.println("보낸 메시지 : " + inputData);
+                System.out.println("===============");
+                if (inputData.equals("")) {
+                    System.out.println("대화가 종료되었습니다.");
+                    break;
+                }
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/site/metacoding/chat/MyServerSocket.java
+++ src/site/metacoding/chat/MyServerSocket.java
@@ -1,15 +1,22 @@
 package site.metacoding.chat;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MyServerSocket {
 
-    ServerSocket serverSocket;
-    Socket socket;
+    ServerSocket serverSocket;// 리스너(연결=세션)
+    Socket socket;// 메시지 통신
     BufferedReader reader;
+
+    // 추가(클라이언트에게 메시지 보내기)
+    BufferedWriter writer;
+    Scanner sc;
 
     public MyServerSocket() {
 
@@ -20,9 +27,41 @@ public class MyServerSocket {
             socket = serverSocket.accept();// while을 돌면서 대기 (내부적)
             reader = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()));
-            String inputData = reader.readLine();
-            System.out.println("받은 메시지 : " + inputData);
             System.out.println("클라이언트 연결됨");
+
+            // 메시지 보내기
+            sc = new Scanner(System.in);
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        String inputData = sc.nextLine();
+                        writer.write(inputData + "\n");
+                        writer.flush();
+                        System.out.println("보낸 메시지 : " + inputData);
+                        System.out.println("===============");
+                        if (inputData.equals("")) {
+                            System.out.println("대화가 종료되었습니다.");
+                            break;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }).start();
+
+            // 메시지 반복해서 받는 서버 소켓 - 메인 스레드
+            while (true) {
+                String inputData = reader.readLine();
+                System.out.println("받은 메시지 : " + inputData);
+                System.out.println("===============");
+                if (inputData.equals("")) {
+                    System.out.println("대화가 종료되었습니다.");
+                    break;
+                }
+            }
         } catch (Exception e) {
             System.out.println("통신 오류 발생 : " + e.getMessage());
             e.printStackTrace();
